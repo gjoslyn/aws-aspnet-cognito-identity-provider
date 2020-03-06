07e4d8d2c9e29d88c3d0e44556ba5aef1b1c0f99
gajoslyn <gajoslyn@CORP.AMAZONWORKSPACES.COM>
2020-03-05 18:46:32 -0700

    initial commit with identity changes

--- samples/Samples/Pages/Index.cshtml.cs
+++ samples/Samples/Pages/Index.cshtml.cs
@@ -1,17 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Text;
 
 namespace Samples.Pages
 {
     public class IndexModel : PageModel
     {
+        public string claimsHtml { get; private set; } = "None";
+
         public void OnGet()
         {
+            if (!User.Identity.IsAuthenticated) return;
 
+            var claims = new StringBuilder("<ul>");
+
+            foreach (var claim in User.Claims)
+            {
+                claims.Append($"<li>{claim.Type} = {claim.Value}</li>");
+            }
+
+            claims.Append("</ul>");
+            claimsHtml = claims.ToString();
         }
     }
 }
